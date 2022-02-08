cfbd63f7fe33e08f7093c876bb0bdef3682216b9
Dev-Rehman <99285270+Dev-Rehman@users.noreply.github.com>
2022-02-08 15:50:41 -0500

    context api testing

---
+++ src/comp/Component1.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState, createContext, useContext } from "react";
+import Component2 from './Component2';
+
+const UserContext = createContext();
+
+export default function Component1() {
+
+    const [user, setUser] = useState("Abdul Rehman");
+  
+    return (
+        <UserContext.Provider value={user}>
+          <h1>{`Hello ${user}!`}</h1>
+          <Component2 user={user} />
+        </UserContext.Provider>
+      );
+  
+}
